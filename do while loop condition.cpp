a7cc49af41f3de3d9ba89fa00dd97a68e8ed6386
Gauravroy746 <142864653+Gauravroy746@users.noreply.github.com>
2023-09-11 15:12:54 +0530

    Add files via upload

---
+++ do while loop condition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    unsigned long n;
+    do{
+        cout<<"enter number:";
+        cin>>n;
+        cout<<"entered:"<<n<<"\n";
+    }while(n!=0);
+    return 0;
+}
