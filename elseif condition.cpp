5ca06ba7d67846e122f58eb05ac56e779bdebc8c
Gauravroy746 <142864653+Gauravroy746@users.noreply.github.com>
2023-09-11 14:34:23 +0530

    Add files via upload

---
+++ elseif condition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int time=22;
+    if (time<10){
+        cout<<"happy";
+    }else if(time<20){
+        cout<<"sad";
+    }else{
+        cout<<"angry";
+    }return 0;
+}
