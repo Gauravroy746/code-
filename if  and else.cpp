37b6665da625900cc419065be9aa4f780c3cd491
Gauravroy746 <142864653+Gauravroy746@users.noreply.github.com>
2023-09-11 14:24:47 +0530

    Add files via upload

---
+++ if  and else.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int time = 35;
+    if(time>60){
+        cout<<"good morning";
+    }
+    else{
+        cout<<"good evening";
+    }return 0;
+}
